17f4c86b7b65099a412c48cdb349ab1407e192e2
MiccDev <54465951+MiccDev@users.noreply.github.com>
2021-06-28 07:14:59 +1200

    Create Vector2.js

---
+++ src/gameobject/Vector2.js
@@ -0,0 +1,16 @@
+class Vector2 {
+
+    /**
+     * The Vector2 class is class used to shorten
+     * x and y into vectors.
+     * @param {number} x 
+     * @param {number} y 
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+export default Vector2;
